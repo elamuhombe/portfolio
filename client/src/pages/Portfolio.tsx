7920ede652c3faec042378c3af6a861fd29ca381
Elaine Muhombe <emuhombe@gmail.com>
2024-07-12 13:49:04 +0300

    feat(src/pages/): Add 'Portfolio.tsx'

---
+++ client/src/pages/Portfolio.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Define the Portfolio component
+const Portfolio: React.FC = () => {
+    return (
+        <div>
+         
+        </div>
+    );
+};
+
+// Export the About component for use in other parts of the app
+export default Portfolio;
