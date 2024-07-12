937baffe0b9b3aeda62ce9ab9683188c74c4e397
Elaine Muhombe <emuhombe@gmail.com>
2024-07-12 14:06:11 +0300

    feat(src/pages/):Export About, Portfolio, and Skills components

---
+++ client/src/pages/index.ts
@@ -0,0 +1,6 @@
+// Exporting the About component from About.tsx
+export { default as About } from './About';
+// Exporting the Portfolio component from Portfolio.tsx
+export { default as Portfolio } from './Portfolio';
+// Exporting the Skills component from Skills.tsx
+export { default as Skills } from './Skills';
