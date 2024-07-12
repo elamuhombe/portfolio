99fed8b22112ec7ec501de0b0c13888339519074
Elaine Muhombe <emuhombe@gmail.com>
2024-07-12 14:26:08 +0300

    feat(src/components/Header/): Add skeleton for Header component

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+// src/components/Header/Header.tsx
+import React from 'react';
+
+// Define the Header component
+const Header: React.FC = () => {
+    return (
+        <header>
+          
+        </header>
+    );
+};
+
+// Export the Header component for use in other parts of the app
+export default Header;
